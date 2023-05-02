0e795455478202cf920c0d02707c01b085dad475
Iwan Francis <iwanfrancis@gmail.com>
2023-05-02 11:37:50 +0100

    chore: setup linting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
-import React from "react";
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">Hello World!</header>
-    </div>
-  );
+const App = () => {
+	return (
+		<div className='p-8'>
+			<h1 className='text-2xl'>Hello world!</h1>
+		</div>
+	)
 }
 
-export default App;
+export default App
